a9a5eaa1edf1aa4d4e9aacd5f69c7cb3881deeea
David Tejuosho <tejuoshodavid@gmail.com>
2024-02-03 01:22:04 -0500

    more config and wiring

--- src/app/portfolio-profile-photo.tsx
+++ src/app/portfolio-profile-photo.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import {getUserImage, getUserName} from "./lib/user-getter";
 
-export function PortfolioProfilePhoto({userid}: { userid: string }) {
-
+export function PortfolioProfilePhoto({userid, isOnProfile}: { userid: string, isOnProfile: boolean }) {
+    isOnProfile ||= false;
 
     return <>
-        <Image src={getUserImage(userid)} alt="Profile photo of user"/>
+        <Image width={200} height={200} src={getUserImage(userid)} alt="Profile photo of user"/>
         <p>{getUserName(userid)}</p>
     </>;
 }
