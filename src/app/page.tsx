9998301f1236af057f97a91b5baee5c411d9d2af
Robotwizard1018 <battleax1018@gmail.com>
2024-02-03 16:55:01 -0500

    Feed updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
 import React from "react";
-import Feed from "@/app/feed";
+import {Feed} from "@/app/feed";
 import { Portfolio } from "./portfolio";
+import {getPosts} from "@/app/lib/user-getter";
 
 
 export default function Home() {
     return (
         <>
-            <Portfolio userid={""} />
+            <Feed />
         </>
-        
-        
     );
     // TODO: replace with your component to test
 }
--- src/app/portfolio-profile-photo.tsx
+++ src/app/portfolio-profile-photo.tsx
@@ -4,8 +4,8 @@ import {getUserImage, getUserName} from "./lib/user-getter";
 export function PortfolioProfilePhoto({userid, isOnProfile}: { userid: string, isOnProfile?: boolean }) {
     isOnProfile ||= false;
 
-    return <>
+    return <div style={{zIndex: 1000}}>
         <Image width={200} height={200} src={getUserImage(userid)} alt="Profile photo of user"/>
         <h1>{getUserName(userid)}</h1>
-    </>;
+    </div>;
 }
